b26caa814d0fdbd1534586c5422ac1e6197fefb3
infamous-711 <mashrik711@gmail.com>
2023-10-27 18:17:03 +0600

    use .when and .whenData instead of switch statements on Futures

--- lib/main.dart
+++ lib/main.dart
@@ -110,21 +110,19 @@ class TrackArt extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final metadata = ref.watch(metadataProvider);
 
-    final image = switch (metadata) {
-      AsyncData(:final value) => Image.memory(value.art),
-      AsyncError(:final error) => Text("$error"),
-      _ => const CircularProgressIndicator(),
-    };
-
-    return ConstrainedBox(
-      constraints: const BoxConstraints(
-        maxWidth: 300,
-        maxHeight: 300,
+    return metadata.when(
+      data: (value) => ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: 300,
+          maxHeight: 300,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16.0),
+          child: Image.memory(value.art),
+        ),
       ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16.0),
-        child: image,
-      ),
+      error: (error, _) => Text("Error: $error"),
+      loading: () => const CircularProgressIndicator(),
     );
   }
 }
@@ -150,12 +148,13 @@ class MusicName extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final name = switch (ref.watch(metadataProvider)) {
-      AsyncData(:final value) => value.title,
-      AsyncError() =>
-        path.basenameWithoutExtension(ref.watch(currentTrackProvider)),
-      _ => "",
-    };
+    final metadata = ref.watch(metadataProvider);
+    final name = metadata.when(
+      data: (value) => value.title,
+      error: (_, __) =>
+          path.basenameWithoutExtension(ref.watch(currentTrackProvider)),
+      loading: () => "",
+    );
 
     return Padding(
       padding: const EdgeInsets.all(10),
@@ -264,20 +263,11 @@ class PlayNext extends ConsumerWidget {
 
     return IconButton(
       icon: const Icon(Icons.skip_next),
-      onPressed: () {
-        switch (musicFiles) {
-          case AsyncData(:final value):
-            {
-              if ((value.length - 1) != currentIndex && currentIndex >= 0) {
-                playMusic(ref, value, currentIndex + 1);
-              }
-            }
-            break;
-
-          default:
-            return;
+      onPressed: () => musicFiles.whenData((value) {
+        if ((value.length - 1) != currentIndex && currentIndex >= 0) {
+          playMusic(ref, value, currentIndex + 1);
         }
-      },
+      }),
       tooltip: "Play Next",
     );
   }
@@ -295,20 +285,11 @@ class PlayPrevious extends ConsumerWidget {
 
     return IconButton(
       icon: const Icon(Icons.skip_previous),
-      onPressed: () {
-        switch (musicFiles) {
-          case AsyncData(:final value):
-            {
-              if (currentIndex > 0) {
-                playMusic(ref, value, currentIndex - 1);
-              }
-            }
-            break;
-
-          default:
-            return;
+      onPressed: () => musicFiles.whenData((value) {
+        if (currentIndex > 0) {
+          playMusic(ref, value, currentIndex - 1);
         }
-      },
+      }),
       tooltip: "Play Previous",
     );
   }
@@ -420,25 +401,25 @@ class MusicList extends ConsumerWidget {
     final musicFiles = ref.watch(musicFilesProvider);
     final index = ref.watch(indexProvider);
 
-    return switch (musicFiles) {
-      AsyncData(:final value) => ListView.builder(
-          itemCount: value.length,
-          itemBuilder: (context, trackIndex) {
-            String titleName = path.basenameWithoutExtension(value[trackIndex]);
-            return ListTile(
-              title: Text(titleName),
-              onTap: () => playMusic(ref, value, trackIndex),
-              selected: trackIndex == index,
-              selectedColor: Theme.of(context).colorScheme.onPrimary,
-              selectedTileColor: Theme.of(context).colorScheme.primary,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-            );
-          },
-        ),
-      AsyncError(:final error) => Text("Error: $error"),
-      _ => const CircularProgressIndicator()
-    };
+    return musicFiles.when(
+      data: (value) => ListView.builder(
+        itemCount: value.length,
+        itemBuilder: (context, trackIndex) {
+          String titleName = path.basenameWithoutExtension(value[trackIndex]);
+          return ListTile(
+            title: Text(titleName),
+            onTap: () => playMusic(ref, value, trackIndex),
+            selected: trackIndex == index,
+            selectedColor: Theme.of(context).colorScheme.onPrimary,
+            selectedTileColor: Theme.of(context).colorScheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+          );
+        },
+      ),
+      error: (error, _) => Text("Error: $error"),
+      loading: () => const CircularProgressIndicator(),
+    );
   }
 }
